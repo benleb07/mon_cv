639644d36e5588b7d632f6240c87aab2ede44547
benleb07 <78544935+benleb07@users.noreply.github.com>
2021-02-04 14:21:24 +0100

    Add files via upload

---
+++ Mon_CV/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+   <html lang="FR-fr">
+     <head>
+     	<meta charset="utf-8"/>
+     	<link rel="stylesheet" href="style.css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+     	<title>PROFIL</title>
+     </head>
+
+     <body>
+        <div id="banner1">
+            <p>Mon CV en ligne !</p>
+        </div>
+     	<div class="menu">
+            <ul>
+            	<li><a href="index.php">PROFIL</a></li>
+                <li><a href="news.html">EXPERIENCES</a></li>
+                <li><a href="infos.html">COMPETENCES</a></li>
+                <li><a href="contact.php">PROJETS</a></li>
+
+
+            </ul>
+     	</div>
+     	<div class="banner2">
+            <p>Bienvenue sur mon site !</p>
+     	</div>
+
+     	<div class="contenuprincipal">
+          <div class="b_profil">
+           <div class="b_id1">
+        <div>
+            <img class="photo_id" src="icone_id.jpg" alt="" height="250px" width="200px"/>
+        </div>
+        <div class="b_id2">
+     		<p class="prenom">Benjamin</p>
+            <br/>
+
+     		<p class="nom">LEBON</p>
+            <br/>
+
+            <p class="poste">Développeur Web et Mobile</p><br/>
+        </div>
+           </div>
+             <div class="b_field">
+              <fieldset class="f_coor">
+                   <legend class="f_leg">Mes Coordonnées !</legend>
+             <div class="coor1">
+                   <img class="img_adresse" src="adresse.png" alt="" height="25px" width="25px"/><p class="p_adresse">3 Avenue Général Gilles</p><br/>
+             </div>
+             <div class="coor2">
+                   <img class="img_mail" src="mail.png" alt="" height="25px" width="25px"/><a href="mailto:benjamin.lebon07@gmail.com">benjamin.lebon07@gmail.com</a><br/>
+             </div>
+             <div class="coor3">
+                   <img class="img_phone" src="phone.jpg" alt="" height="25px" width="25px"/><p class="p_phone">06.03.65.23.52</p>
+             </div>
+              </fieldset>
+             </div> 
+          </div>
+        </div>
+        <div class="mon_profil">
+          <div class="title1">
+            <h1>Mon Profil.</h1>
+          </div>
+            <div class="main_text1">
+              <p class="p_profil">
+                Passionné par les jeux vidéos et l'informatique en général depuis tout petit, je garde une grande curiosité pour le domaine de l'informatique et plus particulièrement pour la programmation que ce soit pour un site web, un jeu vidéo ou encore une application mobile. Je suis quelqu'un de très motivé et persévérant, quand j'ai un projet en tête je n'abandonne pas avant de l'avoir terminé. Je suis organisé et aime les challenges quand ceux-ci se présentent à moi.  
+            
+              </p>
+            </div>
+        </div>
+
+        <div class="ma_formation">
+            <div class="title2">
+              <h2>Ma Formation.</h2>
+            </div>
+            <div class="main_text2">
+              <p class="p_profil2">2016, LE TAMPON, La Réunion:</p>
+                   <ul class="list_form1">
+                       <li>Baccalauréat Général Scientifique.</li>
+                       <li>Lycée Boisjoly Potier, 14eme km, Le Tampon.</li>
+                   </ul>             
+              </p>
+            </div>
+        </div>
+     	<footer>
+     		<p>LEBON Benjamin, Tous droits réservés.</p>
+
+     	</footer>
+
+     
+
+
+
+     </body>
+
+
+   </html>
